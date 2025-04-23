3b28afee1e1b6449c66da46a14425e802d51ae2a
IconiqCS <sungacharles28@gmail.com>
2025-04-23 15:19:15 +0800

    04-23-25 (I did a console controller logic using logical operators and else if condition.)

---
+++ ConsoleController.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   bool keyEnabled = false;
+   bool keyUp = false;
+   bool keyDown = false;
+   bool keyRight = false;
+   bool keyLeft = false;
+
+   if (keyEnabled) {
+        if (keyUp) {
+            cout << "The player is moving up." << endl;
+        } else if (keyDown) {
+            cout << "The player is moving down." << endl;
+        } else if (keyRight) {
+            cout << "The player is moving right." << endl;
+        } else if (keyLeft) {
+            cout << "The player is moving left." << endl;
+        } else {
+            cout << "The player is not moving." << endl;
+        }
+    }
+}
